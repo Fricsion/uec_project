b441a538cd9508fea1fd6ec79977ee5043b49fea
Tiz <taikikku3291@icloud.com>
2019-12-17 17:57:14 +0900

    お菓子買ったところだけ直した

--- animate2.c
+++ animate2.c
@@ -1,9 +1,10 @@
-#include "img.h"
+#include "func_shape.h"
 #include <math.h>
 #define PI 3.14159265358979
 
 int main(void) {
 	struct color green = {0, 255, 0};
+	struct color red = {255, 0, 0};
 	struct color gray = {200, 200, 200};
 	int rad = 50;
 	
@@ -12,6 +13,8 @@ int main(void) {
 		img_clear();
 		img_fillcircle(gray, i, 100, rad);
 		img_fillcycloid(green, rad, 5);
+		img_filltrochoid(red, rad, rad-100, 3);
+
 		img_write();
 	}
 
