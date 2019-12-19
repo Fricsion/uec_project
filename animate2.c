73be1938a42be39bb03812df2ec40b6ecf3a852b
Tiz <taikikku3291@icloud.com>
2019-12-19 16:38:02 +0900

    now cycloids follows circle moving

--- animate2.c
+++ animate2.c
@@ -14,7 +14,7 @@ int main(void)
 	struct color darkred = {153, 51, 51};
 	int rad = 15;
 
-	int divx = 2 * rad; int divy = 100; // レイヤーのずれの値
+	int divx = 300 / 2 - rad*2*PI(); int divy = 100; // レイヤーのずれの値
 
 	float t;
 	for (t = 0.0; t <= 10*PI(); t += PI()/10)
@@ -25,27 +25,27 @@ int main(void)
 
 
 		if(t <= 4*PI()) {
-			img_fillcircle(indigo, rad * t +divx, rad +divy, rad);
+			img_fillcircle(indigo, rad * t +divx, rad +divy +rad, rad);
 		} else if(t <= 5*PI()) {	
-			img_fillcircle(indigo, rad * 4*PI() + rad * sin(t-4*PI()) +divx,  rad * cos(t-4*PI()) +divy, rad);
+			img_fillcircle(indigo, rad * 4*PI() + rad * sin(t-4*PI()) +divx +rad,  rad * cos(t-4*PI()) +divy, rad);
 		} else if(t <= 9*PI()) {
-			img_fillcircle(indigo, rad * 4*PI()  - rad * (t - 5*PI()) +divx, -rad +divy, rad);
+			img_fillcircle(indigo, rad * 4*PI()  - rad * (t - 5*PI()) +divx, -rad +divy +rad, rad);
 		} else if(t <= 10*PI()) {
-			img_fillcircle(indigo,  - rad * sin(t-9*PI()) +divx, - rad * cos(t-9*PI()) +divy, rad);
+			img_fillcircle(indigo,  - rad * sin(t-9*PI()) +divx, - rad * cos(t-9*PI()) +divy +rad, rad);
 		}
 
 		
 
 
-		img_drawcycloid3(green, rad, 0 +divx, 100-rad +divy, t * 180 / PI());
+		img_drawcycloid3(green, rad, 0 +divx, rad +divy, t * 180 / PI());
 
-		img_drawcycloidpoint2(red, rad, 0 +divx, 100-rad +divy, t * 180 / PI(), rad/3);
-		img_drawtrochoid2(white, 0 +divx, 100-rad +divy, rad, rad*2, t * 180 / PI());
+		img_drawcycloidpoint2(red, rad, 0 +divx, rad +divy, t * 180 / PI(), rad/3);
+		img_drawtrochoid2(white, 0 +divx, rad +divy, rad, rad*2, t * 180 / PI());
 
-		img_drawtrochoidpoint2(darkred, 0 +divx, 100-rad +divy, rad, rad*2, t * 180 / PI(), rad/6);
+		img_drawtrochoidpoint2(darkred, 0 +divx, rad +divy, rad, rad*2, t * 180 / PI(), rad/6);
 
-		img_drawtrochoid2(pink, 0 +divx, 100-rad +divy, rad, rad/2, t * 180 / PI());
-		img_drawtrochoidpoint2(darkred, 0 +divx, 100-rad +divy, rad, rad/2, t * 180 / PI(), rad/6);
+		img_drawtrochoid2(pink, 0 +divx, rad +divy, rad, rad/2, t * 180 / PI());
+		img_drawtrochoidpoint2(darkred, 0 +divx, rad +divy, rad, rad/2, t * 180 / PI(), rad/6);
 
 
 		img_write();
