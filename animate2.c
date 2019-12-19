88829f984d9573c3e5ecd03f56f006b33971a1df
Tiz <taikikku3291@icloud.com>
2019-12-19 16:29:10 +0900

    circle smoothly moves

--- animate2.c
+++ animate2.c
@@ -12,30 +12,40 @@ int main(void)
 	struct color yellow = {251, 208, 29};
 	struct color pink = {255, 192, 203};
 	struct color darkred = {153, 51, 51};
-	int rad = 30;
+	int rad = 15;
+
+	int divx = 2 * rad; int divy = 100; // レイヤーのずれの値
 
 	float t;
-	for (t = 0.0; t < 4*PI(); t += PI()/10)
+	for (t = 0.0; t <= 10*PI(); t += PI()/10)
 	{
 		img_clear();
 
 		img_fillrectangle(gray, 0, 0, 300, 100-rad);
 
 
-		img_fillcircle(indigo, rad * t, 100, rad);
+		if(t <= 4*PI()) {
+			img_fillcircle(indigo, rad * t +divx, rad +divy, rad);
+		} else if(t <= 5*PI()) {	
+			img_fillcircle(indigo, rad * 4*PI() + rad * sin(t-4*PI()) +divx,  rad * cos(t-4*PI()) +divy, rad);
+		} else if(t <= 9*PI()) {
+			img_fillcircle(indigo, rad * 4*PI()  - rad * (t - 5*PI()) +divx, -rad +divy, rad);
+		} else if(t <= 10*PI()) {
+			img_fillcircle(indigo,  - rad * sin(t-9*PI()) +divx, - rad * cos(t-9*PI()) +divy, rad);
+		}
 
-//		img_drawline(black, tan(t), 100, rad * t, rad * t + rad);
+		
 
 
-		img_drawcycloid3(green, rad, 0, 100-rad, t * 180 / PI());
+		img_drawcycloid3(green, rad, 0 +divx, 100-rad +divy, t * 180 / PI());
 
-		img_drawcycloidpoint2(red, rad, 0, 100-rad, t * 180 / PI(), 10);
-		img_drawtrochoid2(white, 0, 100-rad, rad, rad*2, t * 180 / PI());
+		img_drawcycloidpoint2(red, rad, 0 +divx, 100-rad +divy, t * 180 / PI(), rad/3);
+		img_drawtrochoid2(white, 0 +divx, 100-rad +divy, rad, rad*2, t * 180 / PI());
 
-		img_drawtrochoidpoint2(darkred, 0, 100-rad, rad, rad*2, t * 180 / PI(), 5);
+		img_drawtrochoidpoint2(darkred, 0 +divx, 100-rad +divy, rad, rad*2, t * 180 / PI(), rad/6);
 
-		img_drawtrochoid2(pink, 0, 100-rad, rad, rad/2, t * 180 / PI());
-		img_drawtrochoidpoint2(darkred, 0, 100-rad, rad, rad/2, t * 180 / PI(), 5);
+		img_drawtrochoid2(pink, 0 +divx, 100-rad +divy, rad, rad/2, t * 180 / PI());
+		img_drawtrochoidpoint2(darkred, 0 +divx, 100-rad +divy, rad, rad/2, t * 180 / PI(), rad/6);
 
 
 		img_write();
