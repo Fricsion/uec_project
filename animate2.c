6bdcf2773d84b6a2ff754ff84c97ea17d4e11d56
Tiz <taikikku3291@icloud.com>
2019-12-20 23:40:56 +0900

    a line shows how the circle rotate

--- animate2.c
+++ animate2.c
@@ -6,6 +6,7 @@ int main(void)
 	struct color green = {0, 255, 0};
 	struct color red = {255, 0, 0};
 	struct color gray = {200, 200, 200};
+	struct color deepbrown = {153, 76, 0};
 	struct color white = {255, 255, 255};
 	struct color indigo = {29, 49, 86};
 	struct color black = {0, 0, 0};
@@ -22,26 +23,30 @@ int main(void)
 	{
 		img_clear();
 
-		img_fillRectangle(gray, 0, 0, 300, 100);
+//		img_fillRectangle(gray, 0, 0, 300, 100);
 
-		img_drawLinePolar(red, 30, 30, 10, PI()/4);
+		img_fillRectangle(deepbrown, 0, 0, 300, 100);
 
 		// パラメータ変数 t で場合わけ
 		if (t <= 4 * PI())
 		{
 			img_fillCircle(indigo, rad * t + divx, rad + divy, rad);
+			img_drawLinePolar(white, rad*t +divx, rad +divy, rad, 3*PI()/2-t);
 		}
 		else if (t <= 5 * PI())
 		{
 			img_fillCircle(indigo, rad * 4 * PI() + rad * sin(t - 4 * PI()) + divx, rad * cos(t - 4 * PI()) + divy, rad);
+			img_drawLinePolar(white, rad * 4 * PI() + rad * sin(t - 4 * PI()) + divx, rad * cos(t - 4 * PI()) + divy, rad, 3*PI()/2-t);
 		}
 		else if (t <= 9 * PI())
 		{
 			img_fillCircle(indigo, rad * 4 * PI() - rad * (t - 5 * PI()) + divx, -rad + divy, rad);
+			img_drawLinePolar(white, rad * 4 * PI() - rad * (t - 5 * PI()) + divx, -rad + divy, rad, -PI()/2 - t); 
 		}
 		else if (t <= 10 * PI())
 		{
 			img_fillCircle(indigo, -rad * sin(t - 9 * PI()) + divx, -rad * cos(t - 9 * PI()) + divy, rad);
+			img_drawLinePolar(white, -rad * sin(t - 9 * PI()) + divx, -rad * cos(t - 9 * PI()) + divy, rad, -PI()/2 - t);
 		}
 
 		if (t <= 12 * PI())
