e9d4f2c383ede3b5caaf60f291eddfa4e6cc1b75
Tiz <taikikku3291@icloud.com>
2019-12-20 11:56:20 +0900

    little change

--- animate2.c
+++ animate2.c
@@ -35,7 +35,7 @@ int main(void)
 			img_fillcircle(indigo,  - rad * sin(t-9*PI()) +divx, - rad * cos(t-9*PI()) +divy, rad);
 		}
 
-		if(t <= 10*PI()) {
+		if(t <= 12*PI()) {
 			// サイクロイドの軌跡
 			img_drawcycloid3(green, rad, 0 +divx, divy, t * 180 / PI());
 			img_drawcycloidpoint2(red, rad, 0 +divx, divy, t * 180 / PI(), rad/3);
@@ -54,12 +54,12 @@ int main(void)
 				img_drawCycloidPointRev2(red, rad, divx, divy, (t-5*PI()) * 180 / PI(), rad/3);
 
 				//			// ２倍の動きのトロコイド
-				img_drawTrochoidRev(white, 0 +divx, divy, rad, rad*2, (t-6*PI()) * 180 / PI());
-				img_drawTrochoidPointRev2(darkred, 0 +divx, divy, rad, rad*2, (t-6*PI()) * 180 / PI(), rad/6);
+				img_drawTrochoidRev(white, 0 +divx, divy, rad, rad*2, (t-5*PI()) * 180 / PI());
+				img_drawTrochoidPointRev2(darkred, 0 +divx, divy, rad, rad*2, (t-5*PI()) * 180 / PI(), rad/6);
 
 				//			// 1/2倍の動きのトロコイド
-				img_drawTrochoidRev(pink, 0 +divx, divy, rad, rad/2, (t-6*PI()) * 180 / PI());
-				img_drawTrochoidPointRev2(darkred, 0 +divx, divy, rad, rad/2, (t-6*PI()) * 180 / PI(), rad/6);
+				img_drawTrochoidRev(pink, 0 +divx, divy, rad, rad/2, (t-5*PI()) * 180 / PI());
+				img_drawTrochoidPointRev2(darkred, 0 +divx, divy, rad, rad/2, (t-5*PI()) * 180 / PI(), rad/6);
 
 			}
 
